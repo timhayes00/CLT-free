b0e8ad9d6c691cceb52e3be2a0016c24aa317e68
timhayes00 <timhayes00@gmail.com>
2018-04-10 12:30:31 -0400

    updated schema/seeds/jobs, added user.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-  INSERT INTO jobs (creator, job_name, job_description, job_category, budget, email)
-  VALUES ("Foo", "Foo Portfolio", "Hello, my name is Foo and I need an online portfolio of my painting work", "Web Design", 200, "foo@gmail.com");
+  INSERT INTO jobs (creator, job_name, job_description, job_category, email)
+  VALUES ("Foo", "Foo Portfolio", "Hello, my name is Foo and I need an online portfolio of my painting work", "Web Design", "foo@gmail.com");
   
-  INSERT INTO jobs (creator, job_name, job_description, job_category, budget, email)
-  VALUES ("Bob", "Bob's Burgers", "I need a lawyer to defend against a copyright infringement suit", "Legal Services", 1000, "HJonBenjaminFan@gmail.com");
+  INSERT INTO jobs (creator, job_name, job_description, job_category, email)
+  VALUES ("Bob", "Bob's Burgers", "I need a lawyer to defend against a copyright infringement suit", "Legal Services", "HJonBenjaminFan@gmail.com");
   
-  INSERT INTO jobs (creator, job_name, job_description, job_category, budget, email)
-  VALUES ("Foo", "Foo's Food Truck app", "I need an interactive mobile app to allow people to order food from their phone for pickup", "Web Design", 2000, "foo@gmail.com");
+  INSERT INTO jobs (creator, job_name, job_description, job_category, email)
+  VALUES ("Foo", "Foo's Food Truck app", "I need an interactive mobile app to allow people to order food from their phone for pickup", "Web Design", "foo@gmail.com");
   
   INSERT INTO freelancers (freelancer_name, skill_set, img_link, bio, portfolio, linked_in, email, rate_requested)
   VALUES ("Tim Hayes", "Web Design", "https://upload.wikimedia.org/wikipedia/commons/3/3a/Bos_grunniens_at_Yundrok_Yumtso_Lake.jpg", "n00b junior web dev", "http://www.github.com", "http://www.linkedin.com", "tim@gmail.com", 40); 
